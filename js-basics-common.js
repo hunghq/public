5fd78aad6cbd6c7478858faa9529b829c4ce901e
hunghq <profhahung@gmail.com>
2015-08-25 15:00:05 +0900

    Create js-basics-common.js

---
+++ js-basics-common.js
@@ -0,0 +1,15 @@
+var logAppender = function (message) {
+    $('#output').append(message);
+    $('#output').append('<br />');
+};
+
+function log(message) {
+    console.log(message);
+
+    if (typeof logAppender == 'function') {
+        logAppender(message);
+    }
+}
+
+log('Hello Console!');
+
